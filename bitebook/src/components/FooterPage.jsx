bddc3a59cfb7b37ca9fe361bc39fd1ccf1eb0add
kainat <Kainatn002@gmail.com>
2025-03-22 16:14:03 +0100

    added about page and played with colours and links

--- bitebook/src/components/FooterPage.jsx
+++ bitebook/src/components/FooterPage.jsx
@@ -2,12 +2,15 @@ export function FooterPage() {
   return (
     <footer>
       <p className="footer-text">
-        Built with ❤️ by Kainat and Richard. Check out the source code on </p>
-        <a className="github-link" href="https://github.com/kainat-1/BiteBook" target="_blank">
-          GitHub
-        </a>
-        .
+        Built with ❤️ by Kainat and Richard. Check out the source code on{" "}
       
+      <a
+        className="github-link"
+        href="https://github.com/kainat-1/BiteBook"
+        target="_blank">
+        GitHub
+      </a>
+      </p>
     </footer>
   );
 }
--- bitebook/src/pages/About.jsx
+++ bitebook/src/pages/About.jsx
@@ -1,7 +1,43 @@
 function About() {
-    return <h1>About us</h1>;
-  }
-  
-  export default About;
-  
+  return (
+    <>
+      <h1>About us</h1>
+      <p>
+        Welcome to BiteBook Recipes App. BiteBook is your personal recipe app,
+        designed exclusively for you to explore and save all your favorite
+        delicious meals. With a wide range of yummy recipes at your fingertips,
+        you'll find everything from quick snacks to elaborate dinners, tailored
+        to your tastes and preferences. The app allows you to easily browse
+        through various categories, You mark your favorite recipes Edit and
+        delete recipes as well With BiteBook, cooking becomes a delightful and
+        personalized experience, helping you discover new flavors and perfect
+        your culinary skills. Enjoy your cooking journey!
+      </p>
+      <p>
+        <h4>Developers</h4>
+        <ul>
+          <li>
+            Richard Nixon
+            <a
+              href="https://www.linkedin.com/in/richardnixondev/"
+              target="blank"
+            >
+              linkedin
+            </a>
+          </li>
+          <li>
+            Kainat Naseer
+            <a
+              href="https://www.linkedin.com/in/kainat-naseer-523845285//"
+              target="blank"
+            >
+              linkedin
+            </a>
+          </li>
+        </ul>
+      </p>
+    </>
+  );
+}
 
+export default About;
