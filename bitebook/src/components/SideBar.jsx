bb6df63e78a2638542087c22fe728290b65d58a3
kainat <Kainatn002@gmail.com>
2025-03-25 21:32:08 +0100

    added icon tried to add theme but not done yet

--- bitebook/src/components/SideBar.jsx
+++ bitebook/src/components/SideBar.jsx
@@ -1,23 +1,24 @@
+import { Link } from "react-router-dom";
 export function SideBar() {
   return (
     <aside>
-      {/* <ul className="sidebar">
+      <ul className="sidebar">
         <li className="DashBoard">
           <Link to="/dashboard">📊 Dash Board</Link>
         </li>
         <li className="profile">
           <Link to="/profile"> 👤Profile</Link>
         </li>
-        <li className="filter">
+        {/* <li className="filter">
           <Link to="/filter"> 🔽 Filter</Link>
-        </li>
-        <li className="settings">
+        </li> */}
+        {/* <li className="settings">
           <Link to="/settings"> ⚙️Settings</Link>
-        </li>
+        </li> */}
         <li className="theme">
           <Link to="/theme">🎨 Theme</Link>
         </li>
-      </ul> */}
+      </ul>
     </aside>
   );
 }
--- bitebook/src/pages/Filter.jsx
+++ bitebook/src/pages/Filter.jsx
@@ -1,9 +1,8 @@
-export function Filter(){
-    return(
-        <ul>
-            <li>
-                list
-            </li>
-        </ul>
-    )
+export function Filter() {
+  return (
+    <ul>
+      <li>list</li>
+    </ul>
+  );
 }
+export default Filter;
